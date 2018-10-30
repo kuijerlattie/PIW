35e191ccc4bafaeb719d3713e28cc474bdc2a51a
Bart "Kuich" Kuijer <kuijerlattie@gmail.com>
2018-10-30 22:45:11 +0100

    xp start

---
+++ PaperInfinityWars/Assets/Scripts/Characters/Player/LevelSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour {
+
+    private int xP;
+    public int currentLevel;
+    private int xPForNextLevel;
+    private int MaxLevel = 60;
+
+	// Use this for initialization
+	void Start () {
+        CalculateCurrentLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void GainXP(int oXP)
+    {
+        xP += oXP;
+        //show xp popup?
+        CalculateCurrentLevel();
+    }
+
+    void CalculateCurrentLevel()
+    {
+        
+    }
+}
