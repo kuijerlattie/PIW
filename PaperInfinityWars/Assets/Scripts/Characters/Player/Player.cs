aacb8b54303446544985641a2db59107899d6187
Bart "Kuich" Kuijer <kuijerlattie@gmail.com>
2018-11-10 19:49:45 +0100

    xp and hitpoints are now visible on screen
    xp and hitpoints now show on screen

--- PaperInfinityWars/Assets/Scripts/Characters/Player/Player.cs
+++ PaperInfinityWars/Assets/Scripts/Characters/Player/Player.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : KillablePawn {
-    
+
+    private int xP;
+    private int currentLevel;
+    private int xPForNextLevel;
+    private int xPForLevelStart;
+    private int MaxLevel = 60;
+
     void Awake()
     {
     }
@@ -12,6 +18,8 @@ public class Player : KillablePawn {
     protected override void Start ()
     {
         base.Start();
+        HitPoints = 3;
+        MaxHitpoints = 3;
         GameManager.instance.SetPlayer(this);
     }
 	
@@ -23,5 +31,52 @@ public class Player : KillablePawn {
     protected override void OnDeath(KillablePawn victim, KillablePawn killer, Weapon weapon)
     {
         GameManager.instance.eventManager.PlayerDeath.Invoke(this, killer, weapon);
+    }
+
+    protected override void OnDamageTaken()
+    {
+        GameManager.instance.eventManager.PlayerHitpointsChanged.Invoke(this);
+    }
+
+    protected override void OnHitpointsChanged()
+    {
+        GameManager.instance.eventManager.PlayerHitpointsChanged.Invoke(this);
+    }
+
+    protected override void OnMaxHitpointsChanged()
+    {
+        GameManager.instance.eventManager.PlayerHitpointsChanged.Invoke(this);
+    }
+
+    void GainXP(int oXP)
+    {
+        xP += oXP;
+        //show xp popup
+        CalculateCurrentLevel();
+    }
+
+    void CalculateCurrentLevel()
+    {
+        GameManager.instance.eventManager.playerXPChanged.Invoke(this);
+    }
+
+    public int GetXP()
+    {
+        return xP;
+    }
+
+    public int GetXPForLevelStart()
+    {
+        return xPForLevelStart;
+    }
+
+    public int GetXPForNextLevel()
+    {
+        return xPForNextLevel;
+    }
+
+    public int GetLevel()
+    {
+        return currentLevel;
     }
 }
